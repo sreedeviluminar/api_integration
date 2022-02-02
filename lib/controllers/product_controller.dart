0dca61152122e697344b6168d97bf52803b65d66
Sreedevi <sreedevi@luminartechnolab.com>
2022-02-02 15:21:07 +0530

    Initial commit

---
+++ lib/controllers/product_controller.dart
@@ -0,0 +1,26 @@
+import 'package:api_integration/services/http_service.dart';
+import 'package:get/get.dart';
+
+class ProductController extends GetxController {
+  var isLoading = true.obs;
+  var productList = [].obs;
+  
+
+  @override
+  void onInit() {
+    fetchProducts();
+    super.onInit();
+  }
+
+  void fetchProducts() async {
+    try {
+      isLoading(true);
+      var products = await HttpService.fetchProducts();
+      if (products != null) {
+        productList.value = products;
+      }
+    } finally {
+      isLoading(false);
+    }
+  }
+}
---
+++ lib/services/http_service.dart
@@ -0,0 +1,18 @@
+import 'package:api_integration/models/product_models.dart';
+import 'package:http/http.dart' as http;
+
+class HttpService {
+  // static var client = http.Client();
+  static Future<List<ProductsModel>> fetchProducts() async {
+    var response =
+        await http.get(Uri.parse("https://fakestoreapi.com/products"));
+    if (response.statusCode == 200) {
+      var data = response.body;
+      return productsModelFromJson(data);
+    } else {
+      // throw Exception();
+      var data = response.body;
+      return productsModelFromJson(data);
+    }
+  }
+}
